88a20cefe9a728a85a40eef0032f010ede5f686c
AaronRasmussen <50007456+AaronRasmussen@users.noreply.github.com>
2025-02-03 07:24:33 -0800

    Initial Commit

---
+++ ARMIDIErrorDevelopmentTests/ARMIDIErrorDevelopmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARMIDIErrorDevelopmentTests.swift
+//  ARMIDIErrorDevelopmentTests
+//
+//  Created by Aaron Rasmussen on 2/3/25.
+//
+
+import Testing
+@testable import ARMIDIErrorDevelopment
+
+struct ARMIDIErrorDevelopmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
